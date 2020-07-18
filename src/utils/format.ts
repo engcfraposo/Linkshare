a051c9dc64ecce4061b2517cb719ef57c34f8770
engcfraposo <engcfraposo@gmail.com>
2020-07-18 16:18:38 -0300

    att

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,4 +1,4 @@
-export const { format: formatPrice } = new Intl.NumberFormat('pt-Br', {
+export const { format: formatPrice } = new Intl.NumberFormat('pt-BR', {
   style: 'currency',
   currency: 'BRL',
 });
